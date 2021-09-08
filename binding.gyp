47cc78cd5ca7c5aa57eef3ca562bb15f7cd62f8e
cristei <cristei.g772@gmail.com>
2021-09-08 14:26:19 +0300

    .clang-format, update binding.gyp

--- binding.gyp
+++ binding.gyp
@@ -2,11 +2,20 @@
     "targets": [
         {
             "target_name": "basil",
-            "target_arch": "x86",
-            "cflags_cc": ["-std=c++17", "-fexceptions"],
-            "sources": ["lib/basil-export.cc"],
-            'include_dirs': ["<!@(node -p \"require('node-addon-api').include\")"],
-            'defines': ["NAPI_CPP_EXCEPTIONS"],
+            "target_arch": "win32",
+            "cflags_cc": [
+                "-std=c++17",
+                "-fexceptions"
+            ],
+            "sources": [
+                "lib/basil-export.cc"
+            ],
+            'include_dirs': [
+                "<!@(node -p \"require('node-addon-api').include\")"
+            ],
+            'defines': [
+                "NAPI_CPP_EXCEPTIONS"
+            ],
             "msvs_configuration_platform": "Win32",
             "msbuild_settings": {
                 "ClCompile": {
--- lib/basil-export.cc
+++ lib/basil-export.cc
@@ -1,27 +1,26 @@
-/*
-*/
+/**
+ * @file basil-export.cc
+ * @author otvv & Cristei Gabriel-Marian (cristei.g772@gmail.com)
+ * @brief basil Bindings to Node JS.
+ * @version 0.1
+ * @date 2021-09-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #include "../basil/basil.hh"
-
 #include <napi.h>
 
 using namespace basil;
-
-namespace basiljs
-{
-
-  //
-  // go nuts.
-  //
-  Napi::Number WRAPPER(const Napi::CallbackInfo &info) {
-
+namespace basiljs {
+Napi::Number WRAPPER(const Napi::CallbackInfo& info) {
     // environment
     Napi::Env env = info.Env();
 
     // check if the arguments provided are valid
-    if (_info.Length() < 1 || !_info[0].IsNumber())
-    {
-      Napi::TypeError::New(env, "GENERIC ERROR MESSAGE HERE:").ThrowAsJavaScriptException();
+    if (_info.Length() < 1 || !_info[0].IsNumber()) {
+        Napi::TypeError::New(env, "GENERIC ERROR MESSAGE HERE:").ThrowAsJavaScriptException();
     }
 
     // create a "variable"
@@ -32,14 +31,13 @@ namespace basiljs
     Napi::Number returnValue = Napi::Number::New(env, function_from_basil_cpp_lib(addressToCall.Int64Value()));
 
     return returnValue;
-  }
 }
+}  // namespace basiljs
 
-Napi::Object Initialize(Napi::Env env, Napi::Object exports)
-{
-  exports.Set("FUNCTION_NAME (this will be the js function name)", Napi::Function::New(_env, WRAPPER));
+Napi::Object Initialize(Napi::Env env, Napi::Object exports) {
+    exports.Set("FUNCTION_NAME (this will be the js function name)", Napi::Function::New(_env, WRAPPER));
 
-  return exports;
+    return exports;
 }
 
 NODE_API_MODULE(basil, Initialize);
