b2bdd055e8e97ed6974a2ebbf156a7fb26c1c3a9
cristei <cristei.g772@gmail.com>
2021-09-19 17:22:18 +0300

    [NOT READY - in-dev] push for backup's sake

--- lib/basil-export.cc
+++ lib/basil-export.cc
@@ -29,8 +29,10 @@
  * @brief This implies only one instance, therefore, basil is exporting a 'set' method.
  * A base import implies a context.
  * 
+ * Lifetime is of global scope.
+ * 
  */
-inline std::shared_ptr<basil::ctx> _ctx;
+inline std::unique_ptr<basil::ctx> _ctx;
 
 /**
  * @brief Connected to _ctx
@@ -41,8 +43,6 @@ namespace wrappers {
 namespace cpp {
     template<typename T>
     T read(uintptr_t at) {
-        static_assert(std::is_integral_v<T>);
-
         const auto read = _ctx->read_memory<T>(at);
         if (read.has_value()) {
             auto [bytes, bytes_read] = read.value();
@@ -63,38 +63,145 @@ namespace cpp {
     }
 }  // namespace cpp
 namespace js {
-    template<typename T>
-    Napi::Value read(const Napi::CallbackInfo& info) {
+    Napi::Function capture_all_modules(const Napi::CallbackInfo& info) {
         const Napi::Env& env = info.Env();
         try {
-            if (info.Length() != 1) {
-                Napi::TypeError::New(env, "Method " __FUNCTION__ " wasn't given either enough arguments.").ThrowAsJavaScriptException();
-            }
-
-            if (!info[0].IsNumber()) {
-                Napi::TypeError::New(env, "Method " __FUNCTION__ " wasn't given an address.").ThrowAsJavaScriptException();
-            }
-
-            return Napi::Number::New(env, cpp::read<T>(info[0].ToNumber().Uint32Value()));
+            _ctx->capture_all_modules();
         } catch (const std::exception& err) {
             Napi::TypeError::New(env, err.what()).ThrowAsJavaScriptException();
             return {};
         }
+
+        return {};
     }
 
-    // inline methods for binding to napi
-    inline Napi::Value read_uint8(const Napi::CallbackInfo& info) {
-        return read<uint8_t>(info);
+    Napi::Value get_module(const Napi::CallbackInfo& info) {
+        const Napi::Env& env = info.Env();
+        Napi::Object ret     = Napi::Object::New(env);
+        try {
+            if (info.Length() != 1) {
+                Napi::TypeError::New(env, __FUNCTION__ ": wasn't given enough arguments.").ThrowAsJavaScriptException();
+            }
+
+            if (!info[0].IsString()) {
+                Napi::TypeError::New(env, __FUNCTION__ ": wasn't given a module.").ThrowAsJavaScriptException();
+            }
+
+            const auto module = _ctx->capture_module(info[0].ToString().Utf8Value());
+            if (module.has_value()) {
+                auto [start, size] = module.value();
+                ret.Set("start", Napi::Number::New(env, (uintptr_t)(start)));
+                ret.Set("size", Napi::Number::New(env, size));
+            }
+        } catch (const std::exception& err) {
+            Napi::TypeError::New(env, err.what()).ThrowAsJavaScriptException();
+            return {};
+        }
+
+        return ret;
     }
+
+    template<typename T>
+    Napi::Value read(const Napi::CallbackInfo& info) {
+        const Napi::Env& env = info.Env();
+        try {
+            bool second  = info.Length() == 2;
+            uintptr_t at = 0;
+
+            if (info.Length() == 1 || second) {
+                if (!info[0].IsNumber()) {
+                    Napi::TypeError::New(env, __FUNCTION__ ": wasn't given an address.").ThrowAsJavaScriptException();
+                }
+            } else {
+                Napi::TypeError::New(env, __FUNCTION__ ": wasn't given enough arguments.").ThrowAsJavaScriptException();
+            }
+
+            if (second) {
+                if (!info[1].IsString()) {
+                    Napi::TypeError::New(env, __FUNCTION__ ": wasn't given a valid string.").ThrowAsJavaScriptException();
+                } else {
+                    const auto module = _ctx->capture_module(info[1].ToString().Utf8Value());
+                    if (module.has_value()) {
+                        auto [start, size] = module.value();
+                        at                 = (uintptr_t)(start);
+                    } else {
+                        throw std::runtime_error(__FUNCTION__ ": Module isn't present.");
+                    }
+                }
+            }
+
+            return Napi::Number::New(env, cpp::read<T>(info[0].ToNumber().Uint32Value() + at));
+        }
+
+        catch (const std::exception& err) {
+            Napi::TypeError::New(env, err.what()).ThrowAsJavaScriptException();
+            return {};
+        }
+
+        return {};
+    }
+
+    Napi::Value pattern_scan(const Napi::CallbackInfo& info) {
+        const Napi::Env& env = info.Env();
+        try {
+            if (info.Length() != 2) {
+                Napi::TypeError::New(env, __FUNCTION__ ": wasn't given enough arguments.").ThrowAsJavaScriptException();
+            }
+
+            if (!info[0].IsArray()) {
+                Napi::TypeError::New(env, __FUNCTION__ ": wasn't given an array.").ThrowAsJavaScriptException();
+            }
+
+            if (!info[1].IsString()) {
+                Napi::TypeError::New(env, __FUNCTION__ ": wasn't given a valid string.").ThrowAsJavaScriptException();
+            }
+
+            const auto module = _ctx->capture_module(info[1].ToString().Utf8Value());
+            if (module.has_value()) {
+                auto [start, size] = module.value();
+                const auto pattern = info[0].As<Napi::ArrayBuffer>();
+                const auto ptr     = _ctx->pattern_scan_module(info[1].ToString().Utf8Value(), pattern.Data(), pattern.ByteLength());
+                if (ptr.has_value()) {
+                    return Napi::Number::New(env, ptr.value());
+                } else {
+                    throw std::runtime_error(__FUNCTION__ ": Pattern scan didn't succeed.");
+                }
+            } else {
+                throw std::runtime_error(__FUNCTION__ ": Module isn't present.");
+            }
+        } catch (const std::exception& err) {
+            Napi::TypeError::New(env, err.what()).ThrowAsJavaScriptException();
+            return {};
+        }
+
+        return {};
+    }
+
+#define MAKE_TYPE(x) \
+    inline Napi::Value read_##x(const Napi::CallbackInfo& info) { \
+        return read<##x>(info); \
+    }
+
+// inline methods for binding to napi
+#define EXECUTE_ALL_API_TYPES(macro) \
+    macro(int8_t); \
+    macro(int16_t); \
+    macro(int32_t); \
+    macro(uint8_t); \
+    macro(uint16_t); \
+    macro(uint32_t); \
+    macro(float);
+
+    EXECUTE_ALL_API_TYPES(MAKE_TYPE);
 }  // namespace js
 }  // namespace wrappers
 
 namespace basiljs {
-// Sets context, returns it in JavaScript.
+// sets context, returns it in JavaScript.
 Napi::Object setter_getter(const Napi::CallbackInfo& info) {
     const Napi::Env& env = info.Env();
     if (info.Length() != 1) {
-        Napi::TypeError::New(env, "Basil setter wasn't given either enough arguments.").ThrowAsJavaScriptException();
+        Napi::TypeError::New(env, "Basil setter wasn't given enough arguments.").ThrowAsJavaScriptException();
     }
 
     if (!info[0].IsString()) {
@@ -104,7 +211,7 @@ Napi::Object setter_getter(const Napi::CallbackInfo& info) {
     Napi::Object ret = Napi::Object::New(env);
     try {
         // set instance to handle passed process
-        _ctx = std::make_shared<basil::ctx>(info[0].ToString());
+        _ctx = std::make_unique<basil::ctx>(info[0].ToString());
 
         // field name from basil::ctx
         ret.Set("name", Napi::String::New(env, _ctx->get_name()));
@@ -117,8 +224,13 @@ Napi::Object setter_getter(const Napi::CallbackInfo& info) {
             throw std::runtime_error("UB: PID wasn't set.");
         }
 
-        // read uint8_t
-        ret.Set("read_uint8", Napi::Function::New(env, wrappers::js::read_uint8));
+        // utility functions
+        ret.Set("get_module", Napi::Function::New(env, wrappers::js::get_module));
+
+// type functions
+// utility macro to run down all types
+#define EXPORT_TYPE(x) ret.Set("read_" #x, Napi::Function::New(env, wrappers::js::read_##x))
+        EXECUTE_ALL_API_TYPES(EXPORT_TYPE);
     } catch (const std::exception& err) {
         Napi::TypeError::New(env, err.what()).ThrowAsJavaScriptException();
         goto as_is;
--- tests/csgo.js
+++ tests/csgo.js
@@ -1,17 +1,28 @@
 'use strict'
 
 // this is an instance
-const basil = require("../lib/basil")
+const process_instance = require("../lib/basil")
 
-(function main() {
-  // capture contents of process named "csgo.exe" in the process list on the instance
-  // returns js struct
-  const instrument = basil.set("csgo.exe")
+// capture contents of process named "csgo.exe" in the process list on the instance
+// returns js struct
+const instrument = process_instance.set("csgo.exe")
 
-  // print process name & pid
-  console.log(instrument.name)
-  console.log(instrument.pid)
+// print process name & pid
+console.log(instrument.name)
+console.log(instrument.pid)
 
-  // read value in memory
-  console.log(instrument.read_uint8(0x268f0000))
-})();
+// print client module base and size
+const client = instrument.get_module("client.dll")
+console.log(client.start)
+console.log(client.size)
+
+// read first 2 bytes of engine.dll (should be 'MZ'), module captured in context.
+console.log(instrument.read_uint16_t(0x0, "engine.dll"))
+
+
+// read first 2 bytes of client.dll (should be 'MZ'), module captured in context already but also
+// available with our already stored js data
+console.log(instrument.read_uint16_t(client.start))
+
+// find pointer to local player position in entity list
+console.log([0x83, 0x3D, -1, -1, -1, -1, -1, 0x75, 0x68, 0x8B, 0x0D, -1, -1, -1, -1, 0x8B, 0x01])
